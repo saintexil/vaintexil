a2f0a20f36f6de3564acc682797583fe1d9c19e3
guitlfromexil <tinoalvio6@gmail.com>
2025-07-05 07:27:00 +0800

    Add files via upload

---
+++ stars/stars.js
@@ -0,0 +1,50 @@
+const canvas = document.getElementById('stars');
+const ctx = canvas.getContext('2d');
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+const stars = [];
+
+for (let i = 0; i < 200; i++) {
+  stars.push({
+    x: Math.random() * canvas.width,
+    y: Math.random() * canvas.height,
+    r: Math.random() * 1.5,
+    d: Math.random() * 0.5
+  });
+}
+
+function drawStars() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.fillStyle = "white";
+  for (let i = 0; i < stars.length; i++) {
+    const s = stars[i];
+    ctx.beginPath();
+    ctx.arc(s.x, s.y, s.r, 0, Math.PI * 2);
+    ctx.fill();
+  }
+}
+
+function moveStars() {
+  for (let i = 0; i < stars.length; i++) {
+    stars[i].y += stars[i].d;
+    if (stars[i].y > canvas.height) {
+      stars[i].y = 0;
+      stars[i].x = Math.random() * canvas.width;
+    }
+  }
+}
+
+function animate() {
+  drawStars();
+  moveStars();
+  requestAnimationFrame(animate);
+}
+
+animate();
+
+window.addEventListener('resize', () => {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+});
